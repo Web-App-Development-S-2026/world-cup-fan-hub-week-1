b58f65eb7f78b6032f6fde70418661beee39f92d
Garv Dugh <n01763558@humber.ca>
2026-06-08 11:17:04 -0700

    Lab 1: Implement About Controller

---
+++ src/main/java/com/cpan228/World/Cup/Event/Hub/controllers/AboutController.java
@@ -0,0 +1,17 @@
+package com.cpan228.World.Cup.Event.Hub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("pageTitle",
+                "Welcome to the 2026 World Cup Hub!");
+
+        return "about";
+    }
+}
