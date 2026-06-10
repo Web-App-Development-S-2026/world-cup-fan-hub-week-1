b704eafebc4ffefcc6864209b9e8daf68c1e658f
Maheen Khan <maheenkhan@Maheens-MacBook-Air.local>
2026-06-10 10:26:54 -0400

    Lab 1: Implement About Controller

---
+++ src/main/java/com/cpan228/World/Cup/Event/Hub/controllers/AboutController.java
@@ -0,0 +1,26 @@
+package com.cpan228.World.Cup.Event.Hub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String about(Model model) {
+
+        model.addAttribute(
+                "pageTitle",
+                "Welcome to the 2026 World Cup Hub!"
+        );
+
+        model.addAttribute(
+                "description",
+                "The 2026 FIFA World Cup will be hosted by Canada, the United States, and Mexico. " +
+                        "This will be the first World Cup hosted by three nations and will feature an expanded tournament format with 48 teams."
+        );
+
+        return "about";
+    }
+}
