bab7ae85979e08f39fbb1e3636ae59edfd0d128f
Milana-Rzazade <https://github.com/Milana-Rzazada>
2026-06-11 19:49:03 -0400

    LAB1: implement About Controller

---
+++ src/main/java/com/cpan228/World/Cup/Event/Hub/controllers/AboutController.java
@@ -0,0 +1,19 @@
+package com.cpan228.World.Cup.Event.Hub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String about(Model model){
+
+        String pageTitle = "Welcome to the 2026 World Cup Hub!";
+
+        model.addAttribute("pageTitle", pageTitle);
+
+        return "about";
+    }
+}
