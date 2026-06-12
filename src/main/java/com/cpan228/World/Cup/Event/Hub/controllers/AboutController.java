7b249d7497a949c9dc98af4198a8b04b33078a1a
Kirandeepkaur8 <kiranbhullara@gmil.com>
2026-06-11 20:34:35 -0400

    Lab 1: About Controller with Thymeleaf

---
+++ src/main/java/com/cpan228/World/Cup/Event/Hub/controllers/AboutController.java
@@ -0,0 +1,18 @@
+package com.cpan228.World.Cup.Event.Hub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AboutController {
+
+    @GetMapping("/about")
+    public String about(Model model) {
+
+        model.addAttribute("pageTitle", "Welcome to the 2026 World Cup Hub!");
+        model.addAttribute("hostCountries", "Canada, USA, and Mexico");
+
+        return "about";
+    }
+}
